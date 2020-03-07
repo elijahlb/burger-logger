2ea6d445a159988d0ab69e0cfe2eea9bbf30e44b
Elijah Broaddus <elijahlb@gmail.com>
2020-03-07 11:42:57 -0500

    test

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,13 @@
 var mysql = require("mysql");
 
+if (process.env.JAWSDB_URL) {
 connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "password", 
-    database: "burgers_db"
+    host: "kil9uzd3tgem3naa.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "bav343nip7vcaul2	",
+    password: "dwak043s4kh524h4", 
+    database: "c3ockqlggdt9focn"
 });
+};
 
 connection.connect(function(err) {
     if(err) {
